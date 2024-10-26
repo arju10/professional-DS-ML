5a237432a2186932eea52fee2471c3d2d3f73e4c
arju10 <arjuarju71@gmail.com>
2024-10-26 11:51:42 +0600

    Find Length of Dictionary

---
+++ Python/Dictionary/length_dictionary.py
@@ -0,0 +1,10 @@
+# len() is used for find the number of key-value pairs in a dictionary
+
+# Create a Dictionary named people_info with various key-value-pairs
+people_info = {'name':'John', 'age':25, 'city':"New York"}
+
+# Getting length of the dictionary
+dict_length = len(people_info)
+
+# Print the created dictionary
+print("Created Dictionary", dict_length)
