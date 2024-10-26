b6192463166cef7fe701010eab57625b09ba02ed
arju10 <arjuarju71@gmail.com>
2024-10-26 11:10:07 +0600

    Create a  Dictionary

---
+++ Python/Dictionary/create_dictionary.py
@@ -0,0 +1,11 @@
+'''
+- A dictionary is a collection of key-value pairs , created using curly braces {}
+- Each key is unique and it is used to access its correspoding values.
+
+'''
+
+# Create a Dictionary named people_info with various key-value-pairs
+people_info = {'name':'John', 'age':25, 'city':"New York"}
+
+# Print the created dictionary
+print("Created Dictionary", people_info)
