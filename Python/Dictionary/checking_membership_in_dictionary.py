6a236110a5a578b318725e2c0eb0e26258a094de
arju10 <arjuarju71@gmail.com>
2024-10-26 11:37:54 +0600

    Checking membership in Dictionary

---
+++ Python/Dictionary/checking_membership_in_dictionary.py
@@ -0,0 +1,11 @@
+# 'in' keyword is used to check a key exists in a dictionary or not
+
+
+# Create a Dictionary named people_info with various key-value-pairs
+people_info = {'name':'John', 'age':25, 'city':"New York"}
+
+# Cheicng membership
+if 'name' in people_info:
+    print('Key"name" is exist in the dictionary')
+else:
+    print('Key"name" is not exist in the dictionary')
