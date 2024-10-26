e39c5c72fcbe26bf47c98365933507a2884403a2
arju10 <arjuarju71@gmail.com>
2024-10-26 11:15:07 +0600

    Modify a  Dictionary

---
+++ Python/Dictionary/modify_dictionary_values.py
@@ -0,0 +1,13 @@
+'''
+- Dictionary are muteable that means their values can be changed by asseing a new value to a key.
+
+'''
+
+# Create a Dictionary named people_info with various key-value-pairs
+people_info = {'name':'John', 'age':25, 'city':"New York"}
+
+# Modify a value
+people_info['age'] = 30
+
+# Print the created dictionary
+print("Created Dictionary", people_info)
