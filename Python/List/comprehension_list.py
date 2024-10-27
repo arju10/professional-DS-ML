6e9466602221998fe6da1a4a843fbb310bce7bc2
arju10 <arjuarju71@gmail.com>
2024-10-27 07:31:33 +0600

    Create Comprehensive list

---
+++ Python/List/comprehension_list.py
@@ -0,0 +1,10 @@
+'''
+- List comprehension is a concise way to cearte lists using an expression and iteration
+- It can be used to generate new lists from existing ones.
+'''
+
+# Create a list of squre of numbers from 0 to 4
+squre_list = [x**2 for x in range(5)]
+
+# Print the  list
+print("Squre List: ", squre_list)
