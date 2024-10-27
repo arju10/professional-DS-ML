10c931a26a0a49aa8fea30ade17ba96d0cbfba2b
arju10 <arjuarju71@gmail.com>
2024-10-27 07:41:00 +0600

    Sorting a list

---
+++ Python/List/sorting_list.py
@@ -0,0 +1,17 @@
+'''
+- sort() is used for sort a list in ascending order.
+- can be descending order by setting the reverse parameter to True.
+'''
+
+
+# Create a list with various data types
+my_list = [10, 20, 30,40, 50]
+
+# Sort by ascending order
+my_list.sort()
+print("Ascending Order list : ", my_list)
+
+# Sort by descending order
+my_list.sort(reverse=True)
+print("Descending Order list : ", my_list)
+
