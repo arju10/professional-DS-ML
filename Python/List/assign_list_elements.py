54ba5be1ebbb6732fdc8cc346977196115b50de7
arju10 <arjuarju71@gmail.com>
2024-10-27 06:54:35 +0600

    Assign list elements

---
+++ Python/List/assign_list_elements.py
@@ -0,0 +1,18 @@
+'''
+- Elements in a list are accessed using indices
+- Indexing starts from 0 (first element) and can also be negative (-1 for the last element).
+'''
+
+# Create a list with various data types
+my_list = [10, 20, 30,40, 50]
+
+# Accesing elements using positive indices
+second_element = my_list[1]
+
+# Accesing elements using negative indices
+last_element = my_list[-1]
+
+
+# Print the accessed elements
+print("Second element: ", second_element)
+print("Last Eleement : ", last_element)
