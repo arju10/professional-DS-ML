c440f1b3efc566cc7ca5cd5886bebc5e79429784
arju10 <arjuarju71@gmail.com>
2024-10-27 07:35:18 +0600

    Check membeership in list

---
+++ Python/List/checking_membership_in_list.py
@@ -0,0 +1,11 @@
+# 'in' keyword is used to check a keyword exists in a list or not
+
+
+# Create a List 
+fruits_list = ['apple','banana','pineapple','orange','cherry']
+
+# Checking membership
+if 'cherry' in fruits_list:
+    print('"cherry" is exist in the dictionary')
+else:
+    print('"cherry" is not exist in the dictionary')
