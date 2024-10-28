1e888d196aec6517faf669a06fb127cbfd32f31a
arju10 <arjuarju71@gmail.com>
2024-10-28 21:34:27 +0600

    Length of Tuple

---
+++ Python/Tuples/length_of_tuple.py
@@ -0,0 +1,10 @@
+# len() is used for find the number of elements in a tuple
+
+# Create tuple
+salary = (100000000000, 25000000000,31200,4242414515)
+
+# Getting length of the tuple
+tuple_length = len(salary)
+
+# Print the created tuple
+print("Length of tuple", tuple_length)
