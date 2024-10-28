80665284381c32a49a2bb60ddbb8fd9fd92a145e
arju10 <arjuarju71@gmail.com>
2024-10-28 21:15:11 +0600

    Removing tuple

---
+++ Python/Tuples/remove_tuple.py
@@ -0,0 +1,18 @@
+'''
+-  Tuple are immutable so element can't be deleted directly
+- For remove a element , you have to convert the tuple in list , and then back to a tuple
+
+'''
+
+
+# Create tuple 
+my_tuple = (1,2,3,'Hello', 4, (10,20))
+
+# Convert a tuple to a list and removing the  elements.
+conv_list = list(my_tuple) # Conver from Tuple to List
+conv_list.remove(2) # Delete the element
+my_tuple = tuple (conv_list) # Conver from List  to Tuple
+
+
+# Print the updated Tuple
+print('Updating Tuple after removing: ', my_tuple)
