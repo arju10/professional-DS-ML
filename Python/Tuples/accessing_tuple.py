5b0c955e95f9f45fbe6d2849eea9ba5da5f3da37
arju10 <arjuarju71@gmail.com>
2024-10-28 20:54:10 +0600

    Accessing tuple element

---
+++ Python/Tuples/accessing_tuple.py
@@ -0,0 +1,17 @@
+'''
+
+- Values in a tuple are accessed using indices, similar to list.
+- Indices start from 0 (first element) and can also be negative (-1 for the last element)
+
+'''
+
+# People Information as tuple format
+salary = (41412412,15412125,1515151)
+
+# Accessing tuple values using indices
+second_element = salary[1]
+last_element = salary [-1]
+
+# Print the accessed values
+print('Second Element : ', second_element)
+print('Last Element : ', last_element)
