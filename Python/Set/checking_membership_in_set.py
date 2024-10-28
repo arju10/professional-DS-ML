f9efc0e9d253246ad670eaeb4b25dffb3c09b315
arju10 <arjuarju71@gmail.com>
2024-10-28 20:37:44 +0600

    Checking membership in set using foor loop

---
+++ Python/Set/checking_membership_in_set.py
@@ -0,0 +1,11 @@
+# 'in' keyword is used to check a keyword exists in a set or not
+
+
+# Create a set 
+fruits_list = {'apple','banana','pineapple','orange','cherry'}
+
+# Checking membership
+if 'cherry' in fruits_list:
+    print('"cherry" is exist in the sets')
+else:
+    print('"cherry" is not exist in the set')
