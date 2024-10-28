f487f7d53de731808099df06ce3a0eb0bf7f61eb
arju10 <arjuarju71@gmail.com>
2024-10-28 20:42:32 +0600

    Length of set

---
+++ Python/Set/set_length.py
@@ -0,0 +1,10 @@
+# len() is used for find the number of elements in a set
+
+# Create set
+salary = {100000000000, 25000000000,31200,4242414515}
+
+# Getting length of the set
+salary_length = len(salary)
+
+# Print the created list
+print("Length of Sets", salary_length)
