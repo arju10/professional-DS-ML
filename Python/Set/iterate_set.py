7fd36cfaec4fd13707748b47ba8e1e75a6f97080
arju10 <arjuarju71@gmail.com>
2024-10-28 20:40:47 +0600

    Iterate set using foor loop

---
+++ Python/Set/iterate_set.py
@@ -0,0 +1,9 @@
+# can be iterate over a set to access its elements
+
+
+# Create a set 
+fruits_list = {'apple','banana','pineapple','orange','cherry'}
+
+# Iterating over keys and values
+for element in fruits_list:
+    print('Fruites: ', element)
