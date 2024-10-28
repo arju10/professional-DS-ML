4147835cb2f0f2fc8966a04c2296105ddac080bb
arju10 <arjuarju71@gmail.com>
2024-10-28 21:20:00 +0600

    Slicing tuple

---
+++ Python/Tuples/slicing_tuple.py
@@ -0,0 +1,14 @@
+'''
+- Slicing allows to extract a part of the tuple
+- It is done using a colon (:) to specify start and step indices
+'''
+
+# Create a tuple with various data types
+my_list = (10, 20, 30,40, 50,60,70)
+
+# Slicing the tuple from index 1 to 4
+slice_list = my_list[1:4]
+
+
+# Print the sliced tuple
+print("Sliced Tuple: ", slice_list)
