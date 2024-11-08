db21a0d7e67528c520c3b29ed4ad759fa20f3695
arju10 <arjuarju71@gmail.com>
2024-11-08 08:33:34 +0600

    Create Array of Zeros & Arrya of Ones

---
+++ Numpy/01_create_numpy_array.py
@@ -0,0 +1,22 @@
+# import neccessary libraries
+import numpy as np # type: ignore
+
+# Create arryas with different function
+array_zeros = np.zeros((3,3))
+array_ones = np.ones((2,2))
+
+
+print("Array of Zeros: \n", array_zeros)
+'''
+Array of Zeros: 
+ [[0. 0. 0.]
+ [0. 0. 0.]
+ [0. 0. 0.]]
+'''
+
+print("Array of ones: \n",array_ones)
+'''
+Array of ones: 
+ [[1. 1.]
+ [1. 1.]]
+'''
